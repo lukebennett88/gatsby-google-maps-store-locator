1268a8ac0f17565f21e0404a1deac50f641a6cac
Luke Bennett <hello@lukebennett.com.au>
2019-11-22 12:14:09 +1100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from 'react-google-maps';
+import * as parkData from '../data/locations.json';
+import mapStyles from '../styles/map-styles';
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === 'Escape') {
+        setSelectedPark(null);
+      }
+    };
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('keydown', listener);
+      return () => {
+        window.removeEventListener('keydown', listener);
+      };
+    }
+
+    return null;
+  }, []);
+
+  return (
+    <GoogleMap
+      defaultZoom={8.5}
+      defaultCenter={{ lat: -37.813629, lng: 144.963058 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {parkData.features.map(park => (
+        <Marker
+          className="font-sans"
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0],
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          // icon={{
+          //   url: `/skateboarding.svg`,
+          //   scaledSize: new window.google.maps.Size(25, 25),
+          // }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0],
+          }}
+        >
+          <div>
+            <h2 className="text-xl">{selectedPark.properties.name}</h2>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
