db5828efed3ce3d4addb38f947d5e6ae339d47ea
Luke Bennett <hello@lukebennett.com.au>
2019-11-24 13:55:02 +1100

    Pull API key from gatsby-config.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import {
   withGoogleMap,
   withScriptjs,
@@ -72,11 +73,20 @@ function Map() {
 const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
-  console.log(process.env.GATSBY_GOOGLE_API_KEY);
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          GOOGLE_API_KEY
+        }
+      }
+    }
+  `);
+  console.log(data.site.siteMetadata.GOOGLE_API_KEY);
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GATSBY_GOOGLE_API_KEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${data.site.siteMetadata.GOOGLE_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
